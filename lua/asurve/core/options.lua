6392a28836ee86c7dd6f65bfb77dc5b9b50123fd
Arnav <arnavsurve@gmail.com>
2024-01-12 10:56:36 -0500

    set up vimtex using skim.app as the default viewer

--- lua/asurve/core/options.lua
+++ lua/asurve/core/options.lua
@@ -18,7 +18,6 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
-
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
---
+++ lua/asurve/plugins/vimtex.lua
@@ -0,0 +1,7 @@
+return {
+  "lervag/vimtex",
+  config = function ()
+    vim.g.vimtex_view_method = 'skim'
+  end
+
+}
