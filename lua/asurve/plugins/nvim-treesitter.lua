d94fc190499545001261c6162a37ca0305ea90a9
Arnav Surve <arnavsurve@gmail.com>
2024-05-23 10:57:31 -0700

    added nvim-treesitter with syntax highlighting support

---
+++ lua/asurve/plugins/nvim-treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  lazy = false,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {"c", "lua", "vim", "html", "css", "javascript", "typescript", "python"},
+      highlight = { enable = true},
+      indent = { enable = true },
+    })
+  end,
+}
