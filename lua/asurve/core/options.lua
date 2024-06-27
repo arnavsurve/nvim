631b6dddeaf6f8bda55d6d7350877f281b1c242a
Arnav Surve <arnav.surve@milgard.com>
2024-06-27 16:00:22 -0700

    restore folds

--- lua/asurve/core/options.lua
+++ lua/asurve/core/options.lua
@@ -34,3 +34,14 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+vim.api.nvim_create_autocmd({"BufWinLeave"}, {
+  pattern = {"*.*"},
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
+  pattern = {"*.*"},
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview"
+})
