4abd0257a19b345cc5af9d1fbcb274e0f7944943
Arnav Surve <arnav@surve.dev>
2025-03-02 15:34:25 -0800

    new colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,18 +5,41 @@ return {
 	},
 
 	{
-		"rebelot/kanagawa.nvim",
+		"sainnhe/edge",
+		lazy = false,
+	},
+
+	{
+		"d00h/nvim-rusticated",
+		lazy = false,
+	},
+
+	{
+		"rose-pine/neovim",
+		name = "rosepine",
+		lazy = false,
+	},
+
+	{
+		"savq/melange-nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme kanagawa-dragon]])
+			vim.cmd([[colorscheme melange]])
 		end,
 	},
+
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+	},
+
 	{
 		"bluz71/vim-moonfly-colors",
 		name = "moonfly",
 		lazy = false,
 	},
+
 	{
 		"vague2k/vague.nvim",
 		name = "vague",
