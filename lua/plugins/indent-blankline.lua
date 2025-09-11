b9bc28feb75fd861fef33e280553f5cce4ef7960
Arnav Surve <arnav@surve.dev>
2025-09-10 23:54:19 -0700

    indent blankline config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,5 +3,20 @@ return {
 	main = "ibl",
 	---@module "ibl"
 	---@type ibl.config
-	opts = {},
+	opts = {
+		indent = {
+			char = "│",
+			highlight = { "IblIndent" },
+		},
+		scope = {
+			enabled = true,
+			char = "│",
+			highlight = { "IblScope" },
+		},
+	},
+	config = function(_, opts)
+		vim.api.nvim_set_hl(0, "IblIndent", { fg = "#2a2a2a" })
+		vim.api.nvim_set_hl(0, "IblScope", { fg = "#7c7c7c" })
+		require("ibl").setup(opts)
+	end,
 }
