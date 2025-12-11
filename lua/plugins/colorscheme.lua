b364cbf440fc0984c42795d831d07aff881bac71
Arnav Surve <arnav@thenetwork.com>
2025-12-10 17:20:28 -0800

    selected tab in tabline -> black text on white bg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,16 @@ return {
 		"arnavsurve/anysphere.nvim",
 		lazy = false,
 		config = function()
+			local function set_tabline_hl()
+				vim.api.nvim_set_hl(0, "TabLineSel", { bg = "#ffffff", fg = "#1e1e1e", bold = true })
+				vim.api.nvim_set_hl(0, "TabLine", { bg = "#2d2d2d", fg = "#808080" })
+				vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#1e1e1e" })
+			end
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				callback = set_tabline_hl,
+			})
 			vim.cmd.colorscheme("anysphere")
+			set_tabline_hl()
 		end,
 	},
 
